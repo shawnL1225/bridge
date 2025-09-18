ebf074b0160a947eb5ef23e4b5906490e6f7fcb4
Shawn <shawnli@ShawndeMacBook-Air.local>
2025-09-18 13:47:18 +0800

    edit font size rwd

--- client/src/config/devConfig.ts
+++ client/src/config/devConfig.ts
@@ -4,7 +4,7 @@ export const DEV_CONFIG = {
   ENABLED: process.env.NODE_ENV === 'development',
   // ENABLED: false,
   // 當前要測試的遊戲狀態
-  GAME_STATE: 'finished' as 'waiting' | 'bidding' | 'playing' | 'finished',
+  GAME_STATE: 'waiting' as 'waiting' | 'bidding' | 'playing' | 'finished',
   
   // 是否跳過大廳直接進入遊戲房間
   SKIP_TO_GAMEROOM: true,
